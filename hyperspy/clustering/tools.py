cf08007f44f397862ad33751e4c799b061fac305
bm424 <bm424@cam.ac.uk>
2016-08-19 13:42:48 +0100

    Improve verbosity of clustering using `tqdm`

--- hyperspy/clustering/tools.py
+++ hyperspy/clustering/tools.py
@@ -1,20 +1,21 @@
 import numpy as np
+import tqdm
 
 
 def try_again(x, q, algorithm, attempts, verbose=False, **kwargs):
     j_best = np.infty
     n = 0
-    while True:
-        if n > attempts:
-            break
+    try_over = range(attempts)
+    if verbose:
+        try_over = tqdm.tqdm(try_over)
+    for n in try_over:
         trial = algorithm(x, q, **kwargs).optimize()
         if trial.J < j_best:
             best_trial = trial
             j_best = trial.J
             if verbose:
-                print(
+                try_over.write(
                     "Trial {}/{}: j_best = {:.2f}".format(n, attempts, j_best))
         elif np.isnan(trial.J):
             continue
-        n += 1
     return best_trial
